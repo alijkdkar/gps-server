d8a90b832896b5d0ce59fcb27b86b544da7b412c
alijkdkar <behzad.bluekian@gmail.com>
2022-11-17 14:59:48 +0330

    redefine redis

--- fileManagerMService/app.py
+++ fileManagerMService/app.py
@@ -4,6 +4,7 @@ from flask import Flask, flash, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
 from flask import current_app
+import base64
 import redis 
 
 
@@ -16,17 +17,36 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # app.config['REDIS_PORT'] = 6379
 # app.config['REDIS_DB'] = 0
 #redis1 = Redis(app)
-redis1 = redis.Redis(host="some-redis",port="6379",db=0)
+
+#redis1 = redis.Redis(host="some-redis",port="6379",db=0)
+redis1 = redis.Redis(host="127.0.0.1",port="6379",db=0)
 
 #todo : comperess image on save
 #todo : comperess image on load
 #todo : face detection on image
-#todoooo : redefine redise db
 #todoooo : save date time of modiy images
 #todo : search on images
 #todo : load images as list
 #todooo : load images as base64
+#Done : redefine redise db
 
+@app.route('/repair', methods=['GET', 'POST'])
+def repair_redis():
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    files =os.listdir(app.config['UPLOAD_FOLDER'])
+    for file in files:
+        
+        if file.count('.')>0:
+            if (redis1.get(str(file).split(".")[0]) or b'').decode("utf-8") != file :
+                redis1.set(str(file).split(".")[0],str(file))
+        else:
+            if (redis1.get(str(file)) or b'').decode("utf-8") != file :
+                redis1.set(str(file),str(file))
+
+    return """{{status:200,msg:"success "}}"""
+        
+    
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -66,23 +86,50 @@ def upload_file():
 
 
 
-
+### begin ### down load 
 
 @app.route('/download/<path:filename>', methods=['GET', 'POST'])
 def download(filename:str):
     if "." in filename:
         return """{{status:200,msg:"wrong file name"}}"""
-    print(filename)
-    orginalFileName = redis1.get(filename)
-    orginalFileName = orginalFileName.decode("utf-8") 
+    orginalFileName = check_res_db(filename) 
     if (orginalFileName or "") == "":
         return """{{status:200,msg:"file not exits"}}"""
-    uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
-    return  send_from_directory(uploads, str(orginalFileName))
+    uploadsurl = getUploadUrl()
+    file = send_from_directory(uploadsurl, str(orginalFileName))
+    return file
+
+
+# @app.route('/downloadBase64/<path:fileID>', methods=['GET', 'POST'])
+# def downloadBase64(fileID:str):
+#     if "." in fileID:
+#         return """{{status:200,msg:"wrong file name"}}"""
+#     db_fileName = check_res_db(fileID)
+#     if (db_fileName or "")=="":
+#         return """{{status:200,msg:"file not exits"}}"""
+#     imagedata=""
+#     uploadsurl = getUploadUrl()
+#     #file = send_from_directory(uploadsurl, str(db_fileName))
+#     print(os.path.join(uploadsurl, str(db_fileName))+".jpeg")
+#     with open(os.path.join(uploadsurl, str(db_fileName)), "wb") as fh:
+#         fh.write(base64.decodebytes(imagedata+".jpeg"))
+#     return imagedata
+
+
+def getUploadUrl():
+    uploadsurl = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
+    return uploadsurl
 
 
 
+def check_res_db(filename):
+    orginalFileName = redis1.get(filename)
+    orginalFileName = orginalFileName.decode("utf-8")
+    return orginalFileName
 
+
+
+### end ### download
 
 
     
